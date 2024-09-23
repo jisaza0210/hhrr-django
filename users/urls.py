ab8d9fd72b41c908b81f4498a065c4789780dbcd
Juan Pablo Isaza M. <jisaza@vaovatravel.com>
2024-09-23 17:20:33 -0500

    feat(login): adds user login and logout

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.home_page, name="home")]
+urlpatterns = [
+    path("", views.home_page, name="home"),
+    path("logout", views.logout_user, name="logout"),
+]
