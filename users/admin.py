848fdcaffd5e56d8fe3cc8b677f373b314795b0e
Juan Isaza <juanisaza@Juans-MacBook-Pro.local>
2024-11-23 16:11:19 -0500

    feat(vacations): adds model

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Team, Employee
+from .models import Team, Employee, Holiday
 
 admin.site.register(Team)
 admin.site.register(Employee)
+admin.site.register(Holiday)
